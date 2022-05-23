9fcdd202afb5e2d74a39c1f1494380e6b732368c
Ellen <ellenkuhm@gmail.com>
2022-05-23 15:11:06 -0400

    Algo HW 1

--- Algo hw/Algo HW1.py
+++ Algo hw/Algo HW1.py
@@ -13,7 +13,7 @@ def maximum (a, b, c):
     list = [a, b, c]
     return max(list)
 
-#print(maximum (124, 21, 32))
+print(maximum (124, 21, 32))
 
 #Count odd and even numbers. Count odd and even digits of the whole number.
 #Example: entered number is 34560, then 3 digits will be even (4, 6, and 0) and 2 odd digits (3 and 5)
